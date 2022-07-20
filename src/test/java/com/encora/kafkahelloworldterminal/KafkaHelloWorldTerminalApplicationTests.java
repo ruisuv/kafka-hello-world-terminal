6247e1f10eea682074810437b0c3b0daecd524a8
Arturo Vargas <arvargas@tripadvisor.com>
2022-07-20 00:45:51 -0500

    Add empty tests.

---
+++ src/test/java/com/encora/kafkahelloworldterminal/KafkaHelloWorldTerminalApplicationTests.java
@@ -0,0 +1,14 @@
+package com.encora.kafkahelloworldterminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// TODO: 20/07/22 Add tests for this later 
+@SpringBootTest
+class KafkaHelloWorldTerminalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
